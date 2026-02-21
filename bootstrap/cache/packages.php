16c5b63d2a51ca3a2632f8a284167b0770d25242
Michal Bursík <michalbursik95@gmail.com>
2026-02-21 21:32:29 +0100

    Upgrade to L12 + PHP 8.4

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -6,34 +6,16 @@
       0 => 'Barryvdh\\LaravelIdeHelper\\IdeHelperServiceProvider',
     ),
   ),
-  'facade/ignition' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Facade\\Ignition\\IgnitionServiceProvider',
-    ),
-    'aliases' => 
-    array (
-      'Flare' => 'Facade\\Ignition\\Facades\\Flare',
-    ),
-  ),
   'flugger/laravel-responder' => 
   array (
-    'providers' => 
-    array (
-      0 => 'Flugg\\Responder\\ResponderServiceProvider',
-    ),
     'aliases' => 
     array (
       'Responder' => 'Flugg\\Responder\\Facades\\Responder',
       'Transformer' => 'Flugg\\Responder\\Facades\\Transformer',
     ),
-  ),
-  'fruitcake/laravel-cors' => 
-  array (
     'providers' => 
     array (
-      0 => 'Fruitcake\\Cors\\CorsServiceProvider',
+      0 => 'Flugg\\Responder\\ResponderServiceProvider',
     ),
   ),
   'laravel/sail' => 
@@ -64,25 +46,15 @@
       0 => 'Laravel\\Tinker\\TinkerServiceProvider',
     ),
   ),
-  'laravel/ui' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\Ui\\UiServiceProvider',
-    ),
-  ),
   'laravel/vapor-core' => 
   array (
+    'aliases' => 
+    array (
+      'Vapor' => 'Laravel\\Vapor\\Vapor',
+    ),
     'providers' => 
     array (
       0 => 'Laravel\\Vapor\\VaporServiceProvider',
-    ),
-  ),
-  'laravel/vapor-ui' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\VaporUi\\VaporUiServiceProvider',
     ),
   ),
   'nesbot/carbon' => 
@@ -97,6 +69,20 @@
     'providers' => 
     array (
       0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'pestphp/pest-plugin-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Pest\\Laravel\\PestServiceProvider',
     ),
   ),
   'spatie/laravel-ray' => 
